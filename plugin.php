f224c4622ef6b3c4503509abc86c78f1cea50599
Arseniy Olevskiy <arsidian@gmail.com>
2023-10-24 15:56:10 +0400

    regular commit

--- plugin.php
+++ plugin.php
@@ -10,6 +10,8 @@ License: GPLv2 or later
 Text Domain: ci_mixed_number_calculator
 */
 
+if (!defined('ABSPATH')) exit;
+
 if (!function_exists('add_shortcode')) return "No direct call for Mixed Number Calculator by Calculator.iO";
 
 function display_ci_mixed_number_calculator(){
